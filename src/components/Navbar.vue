4b2786f5c60bf42a61061d69020278dc56337ec1
oyxiaoxi <oyxiaoxi@hotmail.com>
2023-05-24 18:02:59 +0800

    feat: dark mode

---
+++ src/components/Navbar.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div flex="~ gap2" items-center justify-between px3 pt4 text-xl>
+    <h1 font-serif text-2xl op50>
+      World Time
+    </h1>
+    <DarkSwitcher />
+  </div>
+</template>
